0c9131f1d8e15a23abf5f37728d5407f6194b672
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-06-09 21:27:44 +0500

    Add js timeOut

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 const elModalWindow = document.querySelector(".modal-window");
 const elNotifBtn = document.querySelector("#notif-btn");
-const elModalClose = document.querySelector(".modal-close");
+const elModalClose = document.querySelector(".js-modal-close");
 
-elNotifBtn.addEventListener("click", function () {
-  elModalWindow.classList.add("modal-open");
-});
+if (elModalWindow) {
+  if (elNotifBtn) {
+    elNotifBtn.addEventListener("click", function () {
+      elModalWindow.classList.add("modal-open");
+    });
+  }
 
-elModalClose.addEventListener("click", function () {
-  elModalWindow.classList.remove("modal-open");
-});
+  elModalClose.addEventListener("click", function () {
+    elModalWindow.classList.remove("modal-open");
+  });
+
+  setTimeout(function () {
+    elModalWindow.classList.add("modal-open");
+  }, 3000);
+}
